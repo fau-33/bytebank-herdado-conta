9a9504037986d7f95236ef784bdbb289f3081cf3
Flávio Félix <fau_felix30@hotmail.com>
2023-07-24 16:03:09 -0300

    Modificadores de visibilidades

--- src/br/com/bytebank/banco/test/TesteContas.java
+++ src/br/com/bytebank/banco/test/TesteContas.java
@@ -2,11 +2,15 @@ package br.com.bytebank.banco.test;
 
 import br.com.bytebank.banco.modelo.ContaCorrente;
 import br.com.bytebank.banco.modelo.ContaPoupanca;
+import br.com.bytebank.banco.especial.*;
 import br.com.bytebank.banco.modelo.SaldoInsuficienteException;
 
 public class TesteContas {
 
 	public static void main(String[] args) throws SaldoInsuficienteException {
+		
+		ContaEspecial ce = new ContaEspecial(123, 5555);
+		ce.getSaldo();
 		
 		ContaCorrente cc = new ContaCorrente(111, 111);
 		cc.deposita(100.0);

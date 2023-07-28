65d4a83cef01f079f5dfc90a07d218979d6fd9a1
Flávio Félix <fau_felix30@hotmail.com>
2023-07-28 14:51:40 -0300

    Adicionando ArrayList do java.util

--- src/br/com/bytebank/banco/test/TesteGuardadorReferencias.java
+++ src/br/com/bytebank/banco/test/TesteGuardadorReferencias.java
@@ -4,7 +4,7 @@ import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
 import br.com.bytebank.banco.modelo.GuardadorDeContas;
 
-public class Teste {
+public class TesteGuardadorReferencias {
 
 	public static void main(String[] args) {
 		GuardadorDeContas guardador = new GuardadorDeContas();
@@ -18,7 +18,7 @@ public class Teste {
 		int tamanho = guardador.getQuantidadeDeElementos();
 		System.out.println(tamanho);
 		
-		Conta ref = guardador.getReferencia(0);
+		Conta ref = guardador.getReferencia(1);
 		System.out.println(ref.getNumero());
 
 	}
